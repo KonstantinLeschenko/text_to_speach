7efa28a2d4cea18020573a3f195d62752ad4280d
KonstantinLeschenko <mypost.kh@gmail.com>
2023-09-17 20:26:48 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  FlutterTts flutterTts = FlutterTts();
+  TextEditingController controller = TextEditingController();
+
+  double volume = 1.0;
+  double pitch = 1.0;
+  double speechRate = 1.0;
+  List<String>? languages;
+  String langCode = "ru_RU";
+
+  @override
+  void initState() {
+    super.initState();
+    init();
+  }
+
+  void init() async {
+    languages = List<String>.from(await flutterTts.getLanguages);
+    setState(() {});
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('flutter TTS'),
+          
+        ),
+        body: Container(
+          margin: const EdgeInsets.only(top: 20),
+          child: Center(
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    SizedBox(
+                    width: 200,
+                    height: 60,
+                    child: TextField(
+                      controller: controller,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Enter Text',
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  ElevatedButton(
+                    onPressed: _speak,
+                    child: const Text("Speak"),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  ElevatedButton(
+                    onPressed: _stop,
+                    child: const Text("Stop"),
+                  ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void initSetting() async {
+    await flutterTts.setVolume(volume);
+    await flutterTts.setPitch(pitch);
+    await flutterTts.setSpeechRate(speechRate);
+    await flutterTts.setLanguage(langCode);
+    
+  }
+
+
+  void _speak() async {
+    initSetting();
+    await flutterTts.speak(controller.text);
+  }
+
+  void _stop() async {
+    await flutterTts.stop();
+  }
+}
+
